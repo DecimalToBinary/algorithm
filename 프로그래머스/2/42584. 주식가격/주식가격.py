2d14279588f21ebc574d9b87bd5da76794e2a0e7
DecimalToBinary <76907818+DecimalToBinary@users.noreply.github.com>
2023-11-18 20:43:57 +0900

    [level 2] Title: 주식가격, Time: 63.98 ms, Memory: 16.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -0,0 +1,12 @@
+def solution(prices):
+    times=[0]*len(prices)
+    stack=[]
+    
+    for i in range(len(prices)):
+        for idx,n in stack:
+            times[idx]+=1
+        while stack and stack[-1][1]>prices[i]:
+            stack.pop()
+        stack.append((i,prices[i]))
+    
+    return times
